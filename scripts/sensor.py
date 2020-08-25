3f9da5a30b2416a2c570f0bf926e850a18de9ef2
Emery <abemery@clemson.edu>
2020-08-25 16:11:35 -0400

    tweaking the dropout rates of ttl

--- scripts/sensor.py
+++ scripts/sensor.py
@@ -4,20 +4,22 @@ class sensor:
 
     # initialize the sensor based on type and set age to 0
     def __init__(self, user_defined_sensor_type):
-        type = user_defined_sensor_type
+        self.type = user_defined_sensor_type
+        self.info = None
         age = 0
+        self.meter_id = None
+
         self.ttl = self.determine_ttl()
 
     def getOlder(self):
         self.age += 1
 
-    # determine the life of the sensor
-    def determine_ttl(self):
-        s = np.random.poisson(1, 10000)
+    # determine the life of the sensor (hours)
+    def determine_ttl(self, ):
+        s = np.random.poisson(17532, 1)
         return s
 
-
 if __name__ == '__main__':
-    s = sensor('termostat')
+    s = sensor('thermostat')
     print(np.mean(s.ttl))
     # print(s.determine_ttl('thermostat'))
